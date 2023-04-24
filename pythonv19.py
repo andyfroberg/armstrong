1cc4e0b99334be425fec20b5755c3464aa8cba92
andyfroberg <andy.froberg@gmail.com>
2023-04-23 17:37:30 -0700

    prog

--- pythonv19.py
+++ pythonv19.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import time
 
 
@@ -16,7 +15,6 @@ def main():
             raise OSError(f'Fork failed')
             exit(1)
         elif pid == 0:
-            # pass  #calculate nums
             for n in range(9 + p, num + 1, proc):
                 digits = str(n)
                 num_length = len(digits)
@@ -35,26 +33,26 @@ def main():
             ##### END CITATION #####
         else:  # pid > 0
             child_processes.append(pid)
-            # exit(0)
 
     for pid in child_processes:
         os.waitpid(pid, 0)
 
     program_runtime = round((time.time() - start_time) * 1000)
-    print(f'It took {program_runtime} milliseconds to complete this task.')
+    print(f'It took {program_runtime} milliseconds to complete this task.\n')
     print(f'Armstrong numbers found: {read_child_nums_from_file()}')
     clear_child_nums_files()
 
 
 def write_child_nums_to_file(pid, child_nums):
+    if not os.path.exists(f'nums'):
+        os.mkdir(f'nums')
     with open(f'nums/{pid}.txt', 'w') as f:
-        # f.write(f'[')
         for num in child_nums:
-            f.write(f'{num} ')
-        # f.write(f']')
+            f.write(f'{num}\n')
+
 
 def read_child_nums_from_file():
-    all_nums = ''
+    all_nums = []
     for file in os.listdir(f'nums/'):
         if file == '.DS_Store':
             continue
@@ -62,26 +60,41 @@ def read_child_nums_from_file():
             with open(f'nums/{file}', 'r') as r:
                 nums = r.readlines()
                 for num in nums:
-                    all_nums += f'{num}, '
-    return all_nums[:-2]
+                    # all_nums += f'{num[:-2]}'
+                    all_nums.append(int(num))
+    all_nums.sort()
+    return str(all_nums)[1:-1]
+
 
 def clear_child_nums_files():
     for file in os.listdir(f'nums/'):
         if file != '.DS_Store':
             try:
                 os.remove(f'nums/{file}')
-
+            except OSError as e:
+                print(f'Problem deleting {file} at file path: nums/{file}.')
 
 
 def get_input():
-    # add input validation
-    num = int(input(f'Welcome to the Armstrong number calculator.\n'
-            f'Please enter the maximum number to calculate '
-            f'Armstrong numbers to (10 to 100,000,000): '))
+    print(f'Welcome to the Armstrong number calculator.')
+    while True:
+        try:
+            num = int(input(f'Please enter the maximum number to calculate '
+                            f'Armstrong numbers to (10 to 100,000,000): '))
+            if num < 10 or num > 100_000_000:
+                raise ValueError
+            break
+        except ValueError as e:
+            print(f'You entered an invalid input.')
 
-    # add input validation
-    proc = int(input(f'Please enter the number of processes to use: '))
-    print(f'Numbers per process: {round(num / proc)}')
+    while True:
+        try:
+            proc = int(input(f'Please enter the number of processes to use: '))
+            break
+        except ValueError as e:
+            print(f'You entered an invalid input.')
+
+    print(f'Numbers per process: {round((num - 9)/ proc)}')  # nums doesn't include 1-9
 
     return num, proc
 
